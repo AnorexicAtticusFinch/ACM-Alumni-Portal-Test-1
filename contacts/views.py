258b47a16ad62a0653a6700b454d936f6b827543
AnorexicAtticusFinch <47782857+AnorexicAtticusFinch@users.noreply.github.com>
2020-04-14 17:07:01 +0530

    Add files via upload

--- contacts/views.py
+++ contacts/views.py
@@ -2,32 +2,71 @@ from django.shortcuts import render
 
 from .models import AlumniContact, AlumniContactInterest
 
-class MakeFinContactObj:
+class MakeContactObj:
 
-    def __init__(self, contactObj, interestObjs):
-        self.name = contactObj.name
-        self.gradyear = contactObj.graduation_year
-        self.branch = contactObj.branch
-        self.email = contactObj.email
-        self.phnum = contactObj.phone_number
-        self.curcount = contactObj.current_country
-        self.comp = contactObj.company
-        self.uni = contactObj.university_for_higher_studies
-        self.spec = contactObj.specialisation_for_higher_studies
-        self.moddate = contactObj.last_updated
-        self.interests = [obj.interest for obj in interestObjs]
+    def __init__(self, contactObj_raw, interestObjs_raw):
+        self.name = contactObj_raw.name
+        self.gradyear = contactObj_raw.graduation_year
+        self.branch = contactObj_raw.branch
+        self.email = contactObj_raw.email
+        self.phnum = contactObj_raw.phone_number
+        self.curcount = contactObj_raw.current_country
+        self.comp = contactObj_raw.company
+        self.uni = contactObj_raw.university_for_higher_studies
+        self.spec = contactObj_raw.specialisation_for_higher_studies
+        self.moddate = contactObj_raw.last_updated
+        self.interests = [obj.interest for obj in interestObjs_raw]
+
+    def __lt__(self, operand):
+        return False
+
+def searchForQuery(query, contactObj):
+    query = query.lower()
+    count = 0
+
+    count += contactObj.name.lower().count(query)
+    count += str(contactObj.gradyear).lower().count(query)
+    count += contactObj.branch.lower().count(query)
+    count += contactObj.email.lower().count(query)
+    count += contactObj.phnum.lower().count(query)
+    count += contactObj.curcount.lower().count(query)
+    count += contactObj.comp.lower().count(query)
+    count += contactObj.uni.lower().count(query)
+    count += contactObj.spec.lower().count(query)
+    for interest in contactObj.interests:
+        count += interest.lower().count(query)
+        
+    return count
 
 # Create your views here.
 def testpage_view(request):
 
-    contactObjs = AlumniContact.objects.all()
-    finContactObjs = []
-    for contactObj in contactObjs:
-        interestObjs = AlumniContactInterest.objects.filter(name=contactObj.name)
-        finContactObjs.append(MakeFinContactObj(contactObj, interestObjs))
+    search_queries = request.GET.get("search", "")
+
+    contactObjs_raw = AlumniContact.objects.all()
+    contactObjs = []
+    for contactObj_raw in contactObjs_raw:
+        interestObjs_raw = AlumniContactInterest.objects.filter(name=contactObj_raw.name)
+        contactObjs.append(MakeContactObj(contactObj_raw, interestObjs_raw))
+
+    if search_queries != "":
+        search_queries = search_queries.split(" ")
+        isRelevant = [0 for counter in range(len(contactObjs))]
+        for query in search_queries:
+            for counter in range(len(contactObjs)):
+                isRelevant[counter] += searchForQuery(query, contactObjs[counter])
+    else:
+        isRelevant = [1 for count in range(len(contactObjs))]
+
+    contactObjs_to_be_sorted = []
+    for counter in range(len(contactObjs)):
+        if isRelevant[counter] != 0:
+            contactObjs_to_be_sorted.append((isRelevant[counter], contactObjs[counter]))
+    contactObjs_to_be_sorted.sort(reverse=True)
+    foundContactObjs = [obj[1] for obj in contactObjs_to_be_sorted]
 
     para = {
-        "contacts": finContactObjs,
+        "contacts": foundContactObjs,
     }
 
     return render(request, "testpage.html", para)
